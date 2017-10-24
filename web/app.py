f7e39bd0034e392b471a7d8debf78a4cdc725885
Mike Fullen <mike@mikefullen.com>
2017-10-23 20:22:49 -0400

    login example

--- web/app.py
+++ web/app.py
@@ -9,9 +9,8 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mypassword'
 app.config['MYSQL_DATABASE_DB'] = 'cs6400_sfa17_team033'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_HOST'] = 'mysql'  # mysql is the name of the docker container
-app.config['MYSQL_DATABASE_HOST'] = '192.168.99.100'  # mysql is the name of the docker container
 mysql.init_app(app)
 
 
@@ -23,14 +22,25 @@ def hello():
 @app.route("/customer/login", methods=['POST'])
 def login():
     login_info = request.json
+
+    if login_info['type'] == 'customer':
+        sql_statement = "SELECT password from Customer as c where c.user_name = '{}';".format(login_info['username'])
+    elif login_info['type'] == 'clerk':
+        sql_statement = "SELECT password from Clerk as c where c.user_name = '{}';".format(login_info['username'])
+
     cursor = mysql.connect().cursor()
-    cursor.execute("SELECT password from Customer as c where c.user_name = '{}';".format(login_info['username']))
-    pw = cursor.fetchall()[0][0]
+    cursor.execute(sql_statement)
+    result = cursor.fetchall()
+
+    if cursor.rownumber == 1:
+        pw = result[0][0]
+    else:
+        pw = None
 
     if login_info["password"] == pw:
-        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        return json.dumps({'success': True, 'Type': login_info['type']}), 200, {'ContentType': 'application/json'}
     else:
-        abort(400)
+        return json.dumps({'success': False, 'Type': login_info['type']}), 400, {'ContentType': 'application/json'}
 
 
 @app.route("/myindex")
--- web/static/login/login.js
+++ web/static/login/login.js
@@ -2,22 +2,41 @@
 
 angular.module('myApp.login', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
-    templateUrl: 'static/login/login.html',
-    controller: 'LoginCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'static/login/login.html',
+            controller: 'LoginCtrl'
+        });
+    }])
 
-.controller('LoginCtrl', ['$scope', '$http', 'localStorageService', function($scope, $http, localStorageService) {
-  $scope.view = 'This is a scope variable1';
+    .controller('LoginCtrl', ['$scope', '$http', 'localStorageService', function ($scope, $http, localStorageService) {
+        $scope.view = 'This is a scope variable1';
 
-  $http.post('/customer/login', {"username": "thebatman", "password": "robin"}, { headers: { 'Content-Type': 'application/json' } })
-  .success(function (response) {
-                    localStorageService.set('authorizationData', { userName: response});
+        var vm = this;
+        vm.username = null;
+        vm.password = null;
+        vm.loginType = 'customer';
+
+        vm.canLogin = function() {
+          return vm.username != null &&  vm.password != null;
+        };
+
+        vm.login = function () {
+
+            $http.post('/customer/login', {
+                "username": vm.username,
+                "password": vm.password,
+                "type": vm.loginType
+            }, {headers: {'Content-Type': 'application/json'}})
+                .success(function (response) {
+                    localStorageService.set('authorizationData', {userName: response});
                     console.log('It worked');
+                    console.log(response);
                 })
                 .error(function (err, status) {
                     console.log('Error', err, status);
                 });
-}]);
+        };
+
+
+    }]);
